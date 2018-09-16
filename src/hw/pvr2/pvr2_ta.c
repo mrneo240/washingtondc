c831714f8a7ce3d5bb64799fbf496f2408cfd77d
snickerbockers <chimerasaurusrex@gmail.com>
2018-09-16 00:45:49 -0700

    PVR2: double list-complete time
    The time used is still not based in reality.
    
    This commit fixes galaga in Namco Museum.  Previously this game would miss the
    interrupt and hang forever.

--- src/hw/pvr2/pvr2_ta.c
+++ src/hw/pvr2/pvr2_ta.c
@@ -391,7 +391,7 @@ static bool pvr2_render_complete_int_event_scheduled;
  * which can cause them to miss interrupts.
  *
  */
-#define PVR2_LIST_COMPLETE_INT_DELAY (SCHED_FREQUENCY / 1024)
+#define PVR2_LIST_COMPLETE_INT_DELAY (2 * SCHED_FREQUENCY / 1024)
 
 static void pvr2_op_complete_int_event_handler(struct SchedEvent *event);
 static void pvr2_op_mod_complete_int_event_handler(struct SchedEvent *event);
